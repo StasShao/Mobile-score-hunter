fdb31046ba3d7900e41187c4affddb833b89b3d5
StasShao <saostas@gmail.com>
2023-11-22 00:02:56 +0700

    Five commit

--- ProjectFiles/Prefabs/Spawner/Scripts/Mono/EnemySpawner.cs
+++ ProjectFiles/Prefabs/Spawner/Scripts/Mono/EnemySpawner.cs
@@ -2,34 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using PoolSystems;
+
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private List<Transform> SpawnpositionList;
     protected PoolMono<EnemyCharacter> pool;
     [SerializeField] private EnemyCharacter Prefab;
     [SerializeField] private Transform SpawnContainer;
     [SerializeField] private int SpawnPoolCount;
     [SerializeField] private bool AutoExpand;
     
-
     private void Start()
     {
         pool = new PoolMono<EnemyCharacter>(Prefab,SpawnPoolCount,SpawnContainer,AutoExpand);
-        StartCoroutine(SpawnTimer());
+        for (int i = 0; i < SpawnpositionList.Count; i++)
+        {
+            SpawnpositionList[i].parent = null;
+            SpawnContainer.position = SpawnpositionList[i].position;
+            pool.GetFreeElement().transform.parent = null;
+        }
     }
-    
-    public IEnumerator SpawnTimer()
-    {
-        yield return new WaitForSeconds(5);
-        pool.GetFreeElement();
-        yield return new WaitForSeconds(5);
-        pool.GetFreeElement();
-        yield return new WaitForSeconds(5);
-        pool.GetFreeElement();
-        yield return new WaitForSeconds(5);
-        pool.GetFreeElement();
-        yield return new WaitForSeconds(5);
-        pool.GetFreeElement();
-
-    }
-    
 }
